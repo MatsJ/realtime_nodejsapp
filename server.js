ea1cc93d80a8f49a67400af4d472b7c0a8fc8a9d
Mats Jensen <matsjensen@Matss-MacBook-Pro.local>
2016-03-31 17:41:35 +0200

    basic functionality working

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+//set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//public folder to store stylesheets, javascript, images etc
+app.use(express.static(__dirname + '/public'));
+
+//routes for app
+app.get('/', function(req, res) {
+  res.render('pad');
+});
+
+//listening to port 8000
+var port = process.env.PORT || 8000;
+app.listen(port);
